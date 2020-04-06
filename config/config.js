ce66c89df7bb01bf54232b3528c53c8fb30fa894
Andres Higueros <higueros71@gmail.com>
2020-04-05 23:21:49 -0600

    agregando cambios al archivo de configuracion de ambiente de servicio

--- config/config.js
+++ config/config.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 module.exports = {
     develop: {
         user: process.env.USER_DB || 'root',
         password: process.env.PASS_DB || '',
         port: process.env.PORT || 4000,
-        mongodb: 'mongodb://localhost:27017',
+        mongodb: process.env.MONGO || 'mongodb://localhost:27017',
         status: true
     },
     production: {
-        user: process.env.USER_DB || '',
-        password: process.env.PASS_DB || '',
-        port: process.env.PORT || 4001,
-        mongodb: 'mongodb://localhost:27017',
+        user: process.env.USER_DB,
+        password: process.env.PASS_DB,
+        port: process.env.PORT,
+        mongodb: process.env.MONGO || 'mongodb://localhost:27017',
         status: false
     }
 };
