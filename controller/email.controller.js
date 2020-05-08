dd89957a9a0a02a5410a9e1c67f53edebd7fcf14
Andres Higueros <higueros71@gmail.com>
2020-05-08 04:51:22 -0600

    agregando funcionalidad para poder enviar correo con reporte generado a partir de una data enviada

--- controller/email.controller.js
+++ controller/email.controller.js
@@ -1,9 +1,10 @@
 const config = require('../config/config')
 const sgMail = require('@sendgrid/mail')
 const template = require('../template/template.email')
-const xlsx = require('node-xlsx')
+const excel = require('node-xlsx')
+const fs = require('fs')
+const util = require('../utiles/utiles')
 module.exports = (app, db) => {
-
     return {
         send: (req, res) => {
             sendMail(req, res)
@@ -11,70 +12,62 @@ module.exports = (app, db) => {
     }
 }
 
+const path = './files/reporte-ventas.xlsx'
+
 function sendMail(req, res) {
     try {
-
+        /* get data to create excel file */
         const dataExcel = req.body.data
 
-        if (!dataExcel) {
-            res.json({
-                ok: false,
-                data: null
+        /* delete file */
+        fs.unlinkSync(path)
+        /* create file excel again */
+        let buffer = excel.build([{
+            name: "mySheetName",
+            data: dataExcel
+        }]);
+
+        /* create on directory */
+        fs.writeFile('./files/reporte-ventas.xlsx', new Buffer(buffer, "base64"), (err) => {
+            if (err) throw err;
+            console.log("The file was succesfully saved!");
+            /* read file to send */
+            const fileBase64 = util.base64_encode('./files/reporte-ventas.xlsx')
+
+            /* set configuration of mail */
+            sgMail.setApiKey(config.sendGridKey);
+
+            const msg = {
+                to: req.body.to,
+                from: req.body.from,
+                subject: req.body.subject,
+                text: req.body.text,
+                html: template.TEMPLATE_STRING_BASE,
+                attachments: [{
+                    content: fileBase64,
+                    filename: 'reporte-ventas.xlsx',
+                    type: 'xlsx',
+                    disposition: 'attachment',
+                    contentId: 'test'
+                }, ],
+            };
+            /* send a mail */
+            sgMail.send(msg).then(response => {
+                res.json({
+                    ok: true,
+                    data: response
+                })
+            }).catch(err => {
+                res.json({
+                    ok: false,
+                    data: err
+                })
             })
-        }
-
-        /* generate workbook */
-        /* const ws = XLSX.utils.aoa_to_sheet(dataExcel);
-        const wb = XLSX.utils.book_new();
-        XLSX.utils.book_append_sheet(wb, ws, "reporte-ventas");
-
-        var buffer = XLSX.write(wb, {type:'buffer', bookType: "xlsx"}).toString('base64');
-
-        console.log(buffer); */
-
-        /* if (!buffer) {
-            res.json({
-                ok: false,
-                data: {
-                    message: 'Error, buffer not function'
-                }
-            })
-        } */
-
-        var buffer = xlsx.build([{name: "reporte", data: data}]) // Returns a buffer
-
-        sgMail.setApiKey(config.sendGridKey);
-
-        const msg = {
-            to: req.body.to,
-            from: req.body.from,
-            subject: req.body.subject,
-            text: req.body.text,
-            html: template.TEMPLATE_STRING_BASE,
-            /* attachments: [{
-                content: fileBase64,
-                filename: 'reporte-ventas.xlsx',
-                type: 'xlsx',
-                disposition: 'attachment',
-                contentId: 'test'
-            }, ], */
-        };
-
-        sgMail.send(msg).then(response => {
-            res.json({
-                ok: true,
-                data: response
-            })
-        }).catch(err => {
-            res.json({
-                ok: false,
-                data: err
-            })
-        })
+        });
     } catch (error) {
         res.json({
             message: 'Something is wrong, error ocurred.',
-            error: error
+            error
         })
     }
 }
